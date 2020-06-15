80a6e7cb085b70b66286e0fd729eab58bec037d6
CurseRED <max-cun4@yandex.ru>
2020-06-15 23:21:07 +0300

    error commit

--- src/com/kuntsevich/test/Main.java
+++ src/com/kuntsevich/test/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        dfsg
+                dfg
+                dsf
+                        gsd
+                        fg
+                                sdfg
     }
 }
